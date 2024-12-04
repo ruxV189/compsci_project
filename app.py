d9c085ad97a694ceac1099db1eba1a9fdd909054
ruxV189 <ruxivlieghe67@gmail.com>
2024-12-04 06:06:07 +0000

    worked on login page
    added some CSS, html -> page is coming together slowly

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, Croissant!"
+    return render_template('homepage.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
